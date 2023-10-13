e251adcf5c0db154103ac8ba4ca359f45d5cb85b
wordisstuff <wordisstuff@gmail.com>
2023-10-13 09:44:13 +0300

    task1

---
+++ js/01-gallery.js
@@ -0,0 +1,35 @@
+import { galleryItems } from './gallery-items.js';
+// Change code below this line
+
+const gallery = document.querySelector('.gallery');
+const galleryList = galleryItems.map(({ preview, original, description }) =>
+    `<li class="gallery__item">
+    <a class="gallery__link" href="${original}">
+        <img
+            class="gallery__image"
+            src="${preview}"
+            data-source="${original}"
+            alt="${description}"
+        />
+    </a>
+</li>`).join('');
+
+gallery.insertAdjacentHTML('beforeend', galleryList);
+
+gallery.addEventListener('click', (event) => {
+    event.preventDefault()
+    if (!event.target.classList.contains("gallery__image")) {
+        return;
+    }
+    let instance = basicLightbox.create(`
+    <img src="${event.target.dataset.source}" width="800" height="600">`);
+    instance.show()
+
+    gallery.addEventListener('keydown', event => event.keyCode === 27 ? instance.close() : instance.visible());
+})
+
+
+
+
+
+
---
+++ js/02-lightbox.js
@@ -0,0 +1,3 @@
+import { galleryItems } from './gallery-items.js';
+// Change code below this line
+// console.log(galleryItems);
