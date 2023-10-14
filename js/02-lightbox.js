5a8ed4c3cf14ee7198b74d6c6cc6e4534c6693cb
wordisstuff <wordisstuff@gmail.com>
2023-10-14 18:48:28 +0300

    fix cdn

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,13 +2,7 @@ import { galleryItems } from './gallery-items.js';
 //Change code below this line
 const gallery = document.querySelector('.gallery');
 const galleryList = galleryItems.map(({ preview, original, description }) =>
-    `
-    <li class="gallery__item">
-   <a class="gallery__link" href="${original}">
-      <img class="gallery__image" src="${preview}" alt="${description}" />
-   </a>
-</li>
-`).join('');
+    `<li class="gallery__item"><a class="gallery__link" href="${original}"><img class="gallery__image" src="${preview}" alt="${description}" /></a></li>`).join('');
 
 gallery.insertAdjacentHTML('beforeend', galleryList);
 
