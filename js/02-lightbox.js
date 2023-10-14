8604ad7e4ccc2e07f24dda1e5251b46a3bd0908e
wordisstuff <wordisstuff@gmail.com>
2023-10-14 11:58:29 +0300

    task2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,3 +1,23 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
-// console.log(galleryItems);
+const gallery = document.querySelector('.gallery');
+const galleryList = galleryItems.map(({ preview, original, description }) =>
+    `
+    <li class="gallery__item">
+   <a class="gallery__link" href="${original}">
+      <img class="gallery__image" src="${preview}" alt="${description}" />
+   </a>
+</li>
+`).join('');
+
+gallery.insertAdjacentHTML('beforeend', galleryList);
+
+gallery.addEventListener('click', (event) => {
+    event.preventDefault()
+    if (!event.target.classList.contains("gallery__image")) {
+        return;
+    }
+    let lightBox = new SimpleLightbox('.gallery a', { captionsData: 'alt', captionDelay: 250 })
+})
+
+
